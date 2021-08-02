805dec42a6662830950e5687c2de91caa4c3e88e
Lucas <lucasbottrel03@gmail.com>
2021-08-02 14:35:04 -0300

    Initial Changes - Creating a Language List #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  language: string = 'Spanish';
+  language: string = '';
   listOfLanguages: string[] = ['English', 'Portuguese', 'Indian'];
 
-  addLanguage(){
-    this.listOfLanguages.push(this.language);
-    console.log(this.listOfLanguages);
-    
+  addLanguage() {
+  if (this.language != "") {
+      this.listOfLanguages.push(this.language);
+      console.log(this.listOfLanguages);
+    }
+  }
+
+  deleteFirst(){
+    this.listOfLanguages.splice(0,1);
   }
 }
